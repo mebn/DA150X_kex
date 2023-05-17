90cf10d68f77db89a5b597aaad5f8ea3fdc77deb
Marcus Nilszén <mebn122@gmail.com>
2023-05-17 09:36:40 +0200

    added random seed

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@
 #include <cstring>
 #include <algorithm>
 #include <chrono>
+#include <time.h>
 
 using namespace std::chrono;
 
@@ -26,7 +27,7 @@ const int MAX_STEPS_PER_JOB = 20;
 const int MAX_JOBS = 20;
 const int MAX_MACHINES = 20;
 
-int POPULATION_SIZE = 8000;
+int POPULATION_SIZE = 4000;
 int INDIVIDUAL_LEN = 0;
 const int SIZE_PARENT_POOL = 7;
 
@@ -346,7 +347,9 @@ void stage_2_evaluate_kernel(int *scores, Gene *population, int population_size,
 }
 
 int main(int argc, const char *argv[]) {
-    const char *path = "./data/mk08.fjs";
+    srand(time(NULL));
+
+    const char *path = "./data/mk01.fjs";
     if (argc >= 2) path = argv[1];
     parse_input(path);
 
